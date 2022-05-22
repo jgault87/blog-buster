be6f007746d766722d2352a4d208ad8cce4b2f9a
Audra Gault <jgault87@gmail.com>
2022-05-22 15:22:35 -0600

    added comment class and table

--- models/Blog.js
+++ models/Blog.js
@@ -11,22 +11,19 @@ Blog.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
+    content: {
+      type: DataTypes.TEXT,
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
+
     user_id: {
       type: DataTypes.INTEGER,
       references: {
