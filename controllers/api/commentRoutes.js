fb308207460192c6229259fe3e3a784fea9d0061
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-05-24 13:27:34 -0600

    working on comments rendering

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -10,3 +10,33 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+  Comment.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+module.exports = router;
